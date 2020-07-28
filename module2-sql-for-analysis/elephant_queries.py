57c29fb567c2cf69d2047f7115f3d9dedfb0b772
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-28 08:36:32 -0700

    updated elephant queries

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -1,69 +1,73 @@
 import os
-import psycopg2
 from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import execute_values
+import pandas
 
-load_dotenv()
-
+​load_dotenv() #> loads contents of the .env file into the script's environment
+​
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
-
-### Connect to ElephantSQL-hosted PostgreSQL
-conn = psycopg2.connect(dbname=DB_NAME,
-                        user=DB_USER,
-                        password=DB_PASS, 
-                        host=DB_HOST)
-
-### A "cursor", a structure to iterate over db records to perform queries
-cursor = conn.cursor()
-
-### An example query
-cursor.execute('SELECT * from test_table;')
-#results = cursor.execute('SELECT * from test_table;').fetchone()
-#print(results)
-
-### Note - nothing happened yet! We need to actually *fetch* from the cursor
-results = cursor.fetchall()
-#print(results)
-
-
-########## Connect to SQLite3 DB for RG data ############
-
-import sqlite3
-
-sl_conn = sqlite3.connect("rpg_db.sqlite3")
-sl_cursor = sl_conn.cursor()
-characters = sl_cursor.execute('SELECT * FROM charactercreator_character LIMIT 10')
-#print(characters)
-
-########## Create Character Table in PostGRES ############
-
-create_character_table_query = '''
-CREATE TABLE IF NOT EXISTS rpg_characters (
-    	character_id SERIAL PRIMARY KEY,
-	name VARCHAR(30),
-	level INT,
-	exp INT,
-	hp INT,
-	strength INT,
-	intelligence INT,
-	dexterity INT,
-	wisdom INT
-)
-'''
-
-cursor.execute(create_character_table_query)
-cursor.commit()
-
-########## Insert Character Table in PostGRES ############
-
-for character in characters:
-
-    insert_query = ''' INSERT INTO rpg_characters
-        (character_id, name, level, exp, hp, strength, intelligence, dexterit, wisdom) VALUES
-        {character}
-    '''
-    cursor.execute(insert_query)
-
-conn.commit()
+​
+CSV_FILEPATH = "titanic.csv"
+​
+# CONNECT TO THE PG DATABASE
+​
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+cursor = connection.cursor()
+​
+​
+# CREATE A TABLE TO STORE THE PASSENGERS
+#
+# ... optionally renaming some of the columns, adding a primary key, and changing survived to a bool
+​
+sql = """
+DROP TABLE IF EXISTS passengers;
+CREATE TABLE IF NOT EXISTS passengers (
+    id SERIAL PRIMARY KEY,
+    survived boolean,
+    pclass int4,
+    full_name text,
+    gender text,
+    age int4,
+    sib_spouse_count int4,
+    parent_child_count int4,
+    fare float8
+);
+"""
+cursor.execute(sql)
+​
+#
+# READ PASSENGER DATA FROM THE CSV FILE
+#
+​
+df = pandas.read_csv(CSV_FILEPATH)
+print(df.columns.tolist())
+​
+# to avoid PG insertion errors related to datatype mismatches (psycopg2.ProgrammingError: can't adapt type 'numpy.int64'),
+# ... we need to convert np.int64 columns to normal integers
+# ... https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.astype.html
+# ... https://stackoverflow.com/questions/34838378/dataframe-values-tolist-datatype
+# ... https://stackoverflow.com/questions/47423930/how-to-convert-pandas-dataframe-columns-to-native-python-data-types
+​
+df["Survived"] = df["Survived"].values.astype(bool) # do this before converting to native types, because this actually converts to np.bool
+df = df.astype("object") # converts numpy dtypes to native python dtypes (avoids psycopg2.ProgrammingError: can't adapt type 'numpy.int64')
+​
+#
+# INSERT DATA INTO THE PASSENGERS TABLE
+#
+​
+# how to convert dataframe to a list of tuples
+list_of_tuples = list(df.to_records(index=False))
+​
+insertion_query = f"INSERT INTO passengers (survived, pclass, full_name, gender, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
+execute_values(cursor, insertion_query, list_of_tuples) # third param: data as a list of tuples!
+​
+# CLEAN UP
+connection.commit() # actually save the records / run the transaction to insert rows
+print('Titanic Data successfully saved to Postgres!')
+​
+cursor.close()
+connection.close()
