785e697a24fb0d5ac71d15da5ae9968c0f29a7f2
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-28 22:51:04 -0700

    pymongo testing

---
+++ module3-nosql-and-document-oriented-databases/mongo_explore.py
@@ -0,0 +1,24 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+from pdb import set_trace as breakpoint
+ 
+load_dotenv()
+ 
+DB_USER = os.getenv("MONGO_USER", default="OOPS")
+DB_PASSWORD = os.getenv("MONGO_PASSWORD", default="OOPS")
+CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME", default="OOPS")
+ 
+connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/dspt6-inclass?retryWrites=true&w=majority"
+print("----------------")
+print("URI:", connection_uri)
+ 
+client = pymongo.MongoClient(connection_uri)
+print("----------------")
+print("CLIENT:", type(client), client)
+ 
+# Got this from MongoDB Atlas
+#client = pymongo.MongoClient("mongodb+srv://admin:<password>@<cluster_name>.mongodb.net/<dbname>?retryWrites=true&w=majority")
+#db = client.test
+ 
+breakpoint()
